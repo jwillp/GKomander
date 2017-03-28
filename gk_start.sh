13e0c40d3f10e864fa717b96ccef51c9744ea703
fireraccoon <willyjames.music@gmail.com>
2017-03-27 21:49:35 -0400

    Added basic config for gk to be developped with gk :)

---
+++ gk_start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# This script will run when STARTING the project ""
+# Here you might want to cd into your project directory, activate virtualenvs, etc.
+
+# The currently active project is available via $GK_ACTIVE_PROJECT
+
+git status
