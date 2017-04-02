b9099d754943bd19561ff2e138cc4c3743a4d076
fireraccoon <willyjames.music@gmail.com>
2017-04-01 20:41:30 -0400

    Do not create gk_start and gk_stop when creating a project if it already exists

---
+++ templates/empty.sh
@@ -0,0 +1,5 @@
+# Template representing an empty project
+# $1 name of the project
+# $2 directory containing the project
+# Start and stop scripts already exist. Therefore it is quite easy to
+# append custon commands to the scripts 
