c6fc1d66fce437d7b6e82295f27385a0a7d3a6fc
jwillp <jwilliam.perreault@morebec.com>
2019-07-24 04:27:06 +0200

    Limit project completion to first argument

--- GKomander-completion.sh
+++ GKomander-completion.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
 _gk_projects_completions () { 
-  local cur
-  COMPREPLY=()
-  cur=${COMP_WORDS[COMP_CWORD]}
-  k=0
-  i="$GK_PROJ_DIR" # the directory from where to start
-  for j in $( compgen -f "$i/$cur" ); do # loop trough the possible completions
-    [ -d "$j" ] && j="${j}/" || j="${j} " # if its a dir add a shlash, else a space
-    COMPREPLY[k++]=${j#$i/} # remove the directory prefix from the array
-  done
-  return 0
+	# Limit completion to one arg
+	if [ "${#COMP_WORDS[@]}" != "2" ]; then
+	  return
+	fi
+
+	local cur
+	COMPREPLY=()
+	cur=${COMP_WORDS[COMP_CWORD]}
+	k=0
+	i="$GK_PROJ_DIR" # the directory from where to start
+	for j in $( compgen -f "$i/$cur" ); do # loop trough the possible completions
+		[ -d "$j" ] && j="${j}/" || j="${j} " # if its a dir add a shlash, else a space
+		COMPREPLY[k++]=${j#$i/} # remove the directory prefix from the array
+	done
+	return 0
 }
 
 complete -o nospace -F _gk_projects_completions gk
